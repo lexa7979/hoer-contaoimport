91ad1b54992b7a844b1a4fbe86aff1694f661b73
Alexander Urban <alexander.urban@cepharum.de>
2018-07-06 16:00:58 +0200

    Backend: Access to export module

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,35 +1,25 @@
 <?php
 
 // Add back end modules
-if (! array_key_exists('hoer', $GLOBALS['BE_MOD']))
-	array_insert($GLOBALS['BE_MOD'], 1, array('hoer' => array()));
+$hoer_export = array(
+	'callback'          => 'HoerElectronic\ContaoImport\Export',
+	'tables'            => array(),
+	// 'icon'              => 'system/modules/isotope/assets/images/application-monitor.png',
+	// 'javascript'        => 'system/modules/isotope/assets/js/backend.js',
+);
 
-array_insert($GLOBALS['BE_MOD']['hoer'], 1, array(
-    'export' => array(
-        'callback'          => 'HoerElectronic\ContaoImport\Export',
-        'tables'            => array(),
-        'icon'              => 'system/modules/isotope/assets/images/application-monitor.png',
-        'javascript'        => 'system/modules/isotope/assets/js/backend.js',
-    ),
-));
+if (array_key_exists('isotope', $GLOBALS['BE_MOD'])) {
+	$GLOBALS['BE_MOD']['isotope']['hoer_export'] = $hoer_export;
+}
+else {
+	if (! array_key_exists('hoer', $GLOBALS['BE_MOD']))
+		array_insert($GLOBALS['BE_MOD'], 1, array('hoer' => array()));
+	$GLOBALS['BE_MOD']['hoer']['hoer_export'] = $hoer_export;
+}
 
-if (array_key_exists('ISO_MOD', $GLOBALS) && array_key_exists('miscellaneous:hide', $GLOBALS['ISO_MOD']))
-	$GLOBALS['ISO_MOD']['miscellaneous:hide']['export'] = array(
-		'callback'			=> 'HoerElectronic\ContaoImport\Export',
-		'icon'              => 'system/modules/isotope/assets/images/setup-integrity.png'
-	);
-// (
-// 	'labels' => array
-// 	(
-// 		'tables'            => array(\Isotope\Model\Label::getTable()),
-// 		'icon'              => 'system/modules/isotope/assets/images/setup-labels.png'
-// 	),
-// 	'integrity' => array
-// 	(
-// 		'callback'          => 'Isotope\BackendModule\Integrity',
-// 		'icon'              => 'system/modules/isotope/assets/images/setup-integrity.png'
-// 	),
-// )
+if (array_key_exists('ISO_MOD', $GLOBALS) && array_key_exists('miscellaneous:hide', $GLOBALS['ISO_MOD'])) {
+	$GLOBALS['ISO_MOD']['miscellaneous:hide']['hoer_export'] = $hoer_export;
+}
 
 
 // // Front end module(s):
